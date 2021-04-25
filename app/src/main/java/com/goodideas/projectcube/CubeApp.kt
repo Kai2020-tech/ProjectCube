129b099494c9a72a197306efc159373577a1327b
Kai <kai5266@gmail.com>
2021-04-25 09:41:53 +0800

    dev koin repo,vm

--- app/src/main/java/com/goodideas/projectcube/CubeApp.kt
+++ app/src/main/java/com/goodideas/projectcube/CubeApp.kt
@@ -1,11 +1,40 @@
 package com.goodideas.projectcube
 
 import android.app.Application
+import com.goodideas.projectcube.data.repo.register.IRegisterRepo
+import com.goodideas.projectcube.data.repo.register.RegisterRepo
+import com.goodideas.projectcube.ui.Login.RegisterViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
 import timber.log.Timber
 
 class CubeApp : Application() {
-    override fun onCreate(){
+    override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+
+        val repoModule = module {
+            single<IRegisterRepo> { RegisterRepo(get()) }
+        }
+
+        val vmModule = module {
+            viewModel { RegisterViewModel(get()) }
+        }
+
+        startKoin {
+            androidContext(this@CubeApp)
+            modules(
+                repoModule,
+                vmModule
+            )
+        }
+
     }
 }

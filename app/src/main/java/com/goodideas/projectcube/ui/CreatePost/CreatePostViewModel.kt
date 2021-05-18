3f949099451654f672174f18856db6e78554a0e9
Kai <kai5266@gmail.com>
2021-05-18 15:40:53 +0800

    modify create post,read post api

--- app/src/main/java/com/goodideas/projectcube/ui/CreatePost/CreatePostViewModel.kt
+++ app/src/main/java/com/goodideas/projectcube/ui/CreatePost/CreatePostViewModel.kt
@@ -1,5 +1,6 @@
 package com.goodideas.projectcube.ui.CreatePost
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.goodideas.projectcube.data.network.ApiService
@@ -14,8 +15,8 @@ import java.io.File
 class CreatePostViewModel : ViewModel() {
 
 
-    private fun getPhoto(fileUri: File?): MultipartBody.Part? {
-        val file = File(fileUri?.path ?: "")
+    private fun getPhoto(imageUri: Uri?): MultipartBody.Part? {
+        val file = File(imageUri?.path ?: "")
         return if (file.exists()) {
             val requestFile = file.asRequestBody("image/jpg".toMediaTypeOrNull())
             MultipartBody.Part.createFormData("image", file.name, requestFile)
@@ -25,7 +26,7 @@ class CreatePostViewModel : ViewModel() {
     fun createPost(
         t: MultipartBody.Part,
         c: MultipartBody.Part,
-        imageUri: File?
+        imageUri: Uri?
     ) {
         viewModelScope.launch {
             ApiService.retrofit.createPost(t, c, getPhoto(imageUri))
